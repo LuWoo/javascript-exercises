d4cd5591806f180e28b4f3f7c1a15dde26cf9e47
Luke <lkwood2@gmail.com>
2022-06-20 22:33:15 +0100

    Completed Palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    inputString = inputString.toLowerCase().replace(/[^a-zA-Z0-9]+/g, "");
+    stringArray = inputString.split("");
 
-};
+    for (let i = 0; i <= inputString.length; i++) {
+        
+        if (stringArray[i] != stringArray.reverse()[i]) {
+            return false;
+        }
+    }
+    return true;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
+
