a454f41f8855d21fd70642e367799af9a9bada3e
Luke <lkwood2@gmail.com>
2022-06-20 19:38:35 +0100

    calculator Completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+  const total = inputArray.reduce((sum, current) => {
+    return sum + current
+  }, 0) 
+	return total;
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+ const total = inputArray.reduce((multiply, current) => {
+  return multiply * current;
+ })
+ return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(numberToCalc) {
+  if (numberToCalc <= 0) {
+    return 1
+  }
+  let answer = 1
+  for (let x = 1; x <= numberToCalc; x++){
+    answer = answer * x
+  }
+  return answer
+
 };
 
 // Do not edit below this line
